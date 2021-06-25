dfce201bf5b7a44a622faca79200a0699cac197b
Amgaa <amgaa.khosbayar@gmail.com>
2021-06-24 22:24:31 -0700

    fixed row bug

--- frontend/components/movie/row.jsx
+++ frontend/components/movie/row.jsx
@@ -3,39 +3,37 @@ import React from 'react'
 
 class Row extends React.Component {
     constructor(props) {
-        debugger;
+        // debugger;
         super(props)
-        console.log(props)
         this.handleClick = this.handleClick.bind(this);
         this.handleListButtonClick = this.handleListButtonClick.bind(this);
     }
 
-    // componentDidMount() {
-    //     // debugger;
-    //     this.props.fetchMyLists()
-    // };
+    componentDidMount() {
+        // debugger;
+        this.props.fetchMyLists()
+    };
 
     handleClick(){
         this.props.history.push(`/genre/${this.props.genre.id}`)
     }
 
     handleListButtonClick(id) {
-        // this.props.ids.map((id) => {
             // debugger;
-            if(this.props.movies[id].onMyList === true) {
-                // this.props.mylist.map((id, movie_id) => 
-                //   if (movie_id === this.props.movies[id]{
-                //     this.props.removeMyList(id)
-                //  })); //to remove a mylist it needs a mylist.id?
-            } else {
-                // debugger;
+            if(this.props.movies[id].onMyList !== true) {
                 this.props.createMyList(id);
             }
-        // })
+            // debugger
+            // remove mylist from main page, switch back the icon
+            if(this.props.mylist.length < 0) {
+                this.props.mylist.map((mylistid) => {
+                    this.props.removeMyList(mylistid)
+                })
+            }
     }
 
     render() {
-        debugger;
+        // debugger;
         const videoTiles = this.props.ids.map((id) => {
             let icon;
             if(this.props.movies[id].onMyList === true) {
